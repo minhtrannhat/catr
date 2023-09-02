3402ac506ffffacbd1e2064e8386de2bdbedc697
minhtrannhat <minh@minhtrannhat.com>
2023-09-02 02:31:11 -0400

    Feat(cli): Finished all functionalities
    - passed all tests

--- src/lib.rs
+++ src/lib.rs
@@ -70,11 +70,75 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     }
 }
 
+fn count_digits(mut number: usize) -> usize {
+    if number == 0 {
+        return 1; // Special case for the number 0
+    }
+
+    let mut count = 0;
+
+    while number != 0 {
+        number /= 10;
+        count += 1;
+    }
+
+    count
+}
+
+fn cat(
+    buf_reader: Box<dyn BufRead>,
+    line_num: &mut usize,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+) {
+    for line in buf_reader.lines() {
+        match line {
+            Ok(text) => match (number_lines, number_nonblank_lines) {
+                (true, false) => {
+                    println!(
+                        "{}{}\t{}",
+                        " ".to_string().repeat(6 - count_digits(*line_num)),
+                        line_num,
+                        text
+                    );
+                    *line_num += 1;
+                }
+                (false, true) => {
+                    if text.trim().is_empty() {
+                        println!()
+                    } else {
+                        println!(
+                            "{}{}\t{}",
+                            " ".to_string().repeat(6 - count_digits(*line_num)),
+                            line_num,
+                            text
+                        );
+                        *line_num += 1;
+                    }
+                }
+                _ => {
+                    println!("{}", text);
+                    *line_num += 1;
+                }
+            },
+
+            Err(err) => eprintln!("Error: {}", err),
+        }
+    }
+}
+
 pub fn run(config: Config) -> MyResult<()> {
+    let mut line_num: usize = 1;
+
     for filepath in config.files {
         match open(&filepath) {
             Err(err) => eprintln!("Failed to open {}: {}", filepath, err),
-            Ok(_) => println!("Opened {}", filepath),
+            Ok(buf_reader) => cat(
+                buf_reader,
+                &mut line_num,
+                config.number_lines,
+                config.number_nonblank_lines,
+            ),
         }
     }
 
